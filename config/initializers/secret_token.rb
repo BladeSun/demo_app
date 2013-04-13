60f338b079ad29b46d74650b92567a788a0abe70
root <root@Yibo.(none)>
2013-04-13 13:26:12 +0800

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b281d62cc9dd07e6fec5efd599dfeef3657ec1f30f72dee7962e8721e7cc60c76fbb7e5b7c176304429bfac33174e3c65cc92c74e07c15f84621190b9220491a'
